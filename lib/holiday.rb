b3e61980bcc90a69f03def05f6580d39acb54b80
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-10-03 01:32:25 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,13 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, event|
+    puts "#{season}:"
+    puts "#{event}:"
+    event.each do |supplies|
+      puts "#{supplies}"
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
