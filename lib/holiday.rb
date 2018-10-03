ff45669616dc748cf8610025018304ecbc3ec1cf
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-10-03 01:36:33 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,7 +78,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, event|
     event.each do |supplies|
       if supplies == "BBQ"
-        array << supplies
+        array << season
       end
     end
   end
